4d0860e7ad76e4b9aa76e2dbe8f377caf126c8ea
mariarudneva <mane44ka86@mail.ru>
2018-10-19 22:09:06 +0500

    Перенесла скрипты в один файл

--- source/js/file.js
+++ source/js/file.js
@@ -1,15 +1,29 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 var promoButton = document.querySelectorAll(".promo__button");
 var catalogButton = document.querySelectorAll(".catalog__cart");
 var popup = document.querySelector(".modal");
 var overlay = document.querySelector(".modal__overlay");
 
-promoButton.addEventListener('click', function() {
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function(evt) {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+promoButton.addEventListener('click', function(evt) {
   evt.preventDefault();
   popup.classList.add("modal--show");
   overlay.classList.add("modal__overlay--show");
 });
 
-catalogButton.addEventListener('click', function() {
+catalogButton.addEventListener('click', function(evt) {
   evt.preventDefault();
   popup.classList.add("modal--show");
   overlay.classList.add("modal__overlay--show");
@@ -25,3 +39,4 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
