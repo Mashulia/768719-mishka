d6c58ac0198f2f463f93619283b14698eb38f28e
mariarudneva <mane44ka86@mail.ru>
2018-10-22 08:11:23 +0500

    Проверка состояния

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,6 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-
 gulp.task("copy", function () {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
@@ -104,7 +103,6 @@ gulp.task("refresh", function (done) {
   server.reload();
   done();
 });
-
 
 gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
