ab0572d84690890e8d0652dcabe94a2af5e6f9be
mariarudneva <mane44ka86@mail.ru>
2018-10-29 11:07:26 +0500

    Выправила модалку

--- source/js/file.js
+++ source/js/file.js
@@ -2,7 +2,7 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var link = document.querySelectorAll('.js-modal');
 var popup = document.querySelector('.modal');
-var overlay = document.querySelector(".modal__overlay");
+var overlay = document.querySelector('.modal__overlay');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -19,7 +19,7 @@ navToggle.addEventListener('click', function () {
 for(var i = 0; i < link.length; i++) {
   link[i].addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.add('modal-show');
+    popup.classList.add('modal--show');
   });
 }
 
@@ -27,13 +27,13 @@ window. addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
 
-    if (popup.classList.contains('modal-show')) {
-      popup.classList.remove('modal-show');
+    if (popup.classList.contains('modal--show')) {
+      popup.classList.remove('modal--show');
     }
   }
 });
 
 overlay.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.remove('modal-show');
+  popup.classList.remove('modal--show');
 });
